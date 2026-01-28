56dccc52270de1f26370794ce768583f30dcf167
cesar-martinez-torres <cesar.martinez@udlap.mx>
2026-01-28 10:51:26 -0600

    28 enero

---
+++ Teoria/cinem_28_ene.m
@@ -0,0 +1,9 @@
+%% Ejercicio 1.2
+W=transl(0,0,0)
+cTb=transl(0,0,2)*troty(-180,'deg')*trotz(90,'deg')
+wTb=transl(-1,2,1)
+% Puntos de las esquinas de la mesa
+wp1=[0;1;1]
+wp2=[0;3;1]
+wp3=[-2;3;1]
+wp4=[-2;1;1]
