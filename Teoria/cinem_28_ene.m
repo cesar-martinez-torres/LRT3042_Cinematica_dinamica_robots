a4344e28d4f03c7dce368c793065d8026d44b402
cesar-martinez-torres <cesar.martinez@udlap.mx>
2026-01-28 17:51:50 -0600

    28 enero

--- Teoria/cinem_28_ene.m
+++ Teoria/cinem_28_ene.m
@@ -1,4 +1,6 @@
 %% Ejercicio 1.2
+clear
+close all
 W=transl(0,0,0)
 cTb=transl(0,0,2)*troty(-180,'deg')*trotz(90,'deg')
 wTb=transl(-1,2,1)
@@ -7,3 +9,14 @@ wp1=[0;1;1]
 wp2=[0;3;1]
 wp3=[-2;3;1]
 wp4=[-2;1;1]
+figure
+plot_point3(wp1)
+hold on
+plot_point3(wp2)
+plot_point3(wp3)
+plot_point3(wp4)
+plot3DLine(wp1,wp2)
+trplot(W,'frame','W','arrow')
+%% 2)
+syms aTw wTc aTb
+aTb=aTw*wTc*cTb% Matlab trata a aTw y wTc como escalares
